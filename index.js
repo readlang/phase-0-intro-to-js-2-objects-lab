e6aca4fea09bf6584126680c2d4119d83aa0b9a5
Read Langworthy <rlangworthy@gmail.com>
2021-10-15 17:11:27 -0700

    commit tree c1ce31db25ce509aa1fc1c91d97e707e5962fecd

--- index.js
+++ index.js
@@ -1,44 +1,102 @@
 // Write your solution in this file!
 
 //define employee
-const employee = {name:0, streetAddress:0};
+
+const employee = {
+    name: "Joe",
+    streetaddress: "123 second st",
+}
+
+console.log(employee)
 
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
-    const newObject = {...employee};
-    newObject[key] = value;
-    return newObject;
+    const newObj = {...employee}
+    newObj[key] = value
+    return( newObj )
 }
 
-console.log(employee);
 
-console.log(updateEmployeeWithKeyAndValue(employee, "name", "RJL") )
+
+//console.log( updateEmployeeWithKeyAndValue(employee, "name", "bob") )
 
 
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
+    employee[key] = value
+    return(employee)
 }
-
-console.log(employee);
-
-console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "RJL") )
-
-console.log(employee);
 
 
 function deleteFromEmployeeByKey(employee, key) {
-    const newObject = {...employee};
-    delete newObject[key];
-    return newObject;
+    const newObj = {...employee};
+    delete newObj[key];
+    return (newObj);
 }
 
-console.log(deleteFromEmployeeByKey(employee, "name") )
+console.log( deleteFromEmployeeByKey(employee, "name") )
 
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {name:0, streetAddress:0};
+
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newObject = {...employee};
+//     newObject[key] = value;
+//     return newObject;
+// }
+
+// console.log(employee);
+
+// console.log(updateEmployeeWithKeyAndValue(employee, "name", "RJL") )
+
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee;
+// }
+
+// console.log(employee);
+
+// console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee, "name", "RJL") )
+
+// console.log(employee);
+
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newObject = {...employee};
+//     delete newObject[key];
+//     return newObject;
+// }
+
+// console.log(deleteFromEmployeeByKey(employee, "name") )
+
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+//     return employee;
+// }
+
+
+
+
+
 
 //destructivelyDeleteFromEmployeeByKey(): this function should work the same as deleteFromEmployeeByKey() but it should mutate the employee Object.
